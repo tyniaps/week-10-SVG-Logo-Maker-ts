73de37acf21689e0e5a7fa13c8745ba44b9270e2
KnightNimbus <ty.s.spain@gmail.com>
2024-03-26 22:45:26 +0000

    function for creating logo - logo created will be based on data gathered from user input

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Triangle, Circle, Square} = require('./lib/shapes');
 
+let requestedShape = undefined
+
 console.log("Welcome to the SVG Logo Maker")
 console.log("Please answer the following questions in order to create your logo:")
 
@@ -28,4 +30,19 @@ const questions = [
         message: 'What colour would you like the 3 letters be?'
     },
 ]
+
+function createLogo(data) {
+
+    if (data.logoShape === 'Triangle') {
+        requestedShape = new Triangle(data.logoColor, data.logoText, data.textColor)
+
+    } else if (data.logoShape === 'Circle') {
+        requestedShape = new Circle(data.logoColor, data.logoText, data.textColor)
+    
+    }else {
+        requestedShape = new Square(data.logoColor, data.logoText, data.textColor)
+    
+    } return requestedShape.render();
+
+}
 
